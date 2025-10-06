06c5ec7f6d9d3f73dd8c3b810281c57fd96d7dff
Ernest <ernestkennethyoungsumarno@gmail.com>
2025-10-06 08:16:44 +0100

    updates

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,13 +1,13 @@
 package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 public class Person {
 
     private final UUID id;
-    @NotBlank
+//    @NotBlank
     private final String name;
 
     public Person(@JsonProperty("id") UUID id,
